5568d77f122b1429603fb9a511f14edf17db99e0
Sangita Kumre <sankumre@in.ibm.com>
2020-07-02 13:52:55 +0530

    Updates

---
+++ precache-manifest.9bdbfb2d78d581cb1c43c2e95f234bf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f64e1bba48fbb6aa4198a4bd1058cd",
+    "url": "/react-user/index.html"
+  },
+  {
+    "revision": "97162a41358f2408df0f",
+    "url": "/react-user/static/css/main.fdd4c386.chunk.css"
+  },
+  {
+    "revision": "dbcd1a82aa17efb5919a",
+    "url": "/react-user/static/js/2.c11d35ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-user/static/js/2.c11d35ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97162a41358f2408df0f",
+    "url": "/react-user/static/js/main.152d0b82.chunk.js"
+  },
+  {
+    "revision": "6203a5ad78229c4764e2",
+    "url": "/react-user/static/js/runtime-main.7bc5e0e5.js"
+  },
+  {
+    "revision": "dc094185c67be5458232d756ec9cdae7",
+    "url": "/react-user/static/media/img.dc094185.jpg"
+  }
+]);
